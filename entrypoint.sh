5d622ecd316c94ffc6d9dbbc0744a6b26ce5a384
Max Byszio <maxbyszio@gmail.com>
2022-09-14 18:37:12 +0200

    fix login failure logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,10 @@ if [ $R -ne 0 ]; then
     expect \"accept license (yes/no): \" {send \"yes\n\"}
     expect \"Personal login token: \" {send \"$JOTTA_TOKEN\n\"}
     expect {
+    	eof {
+            puts stderr \"Login failed\"
+            exit 1
+        }
     	\"Devicename*: \" {
 		send \"$JOTTA_DEVICE\n\"
 		expect eof
@@ -47,11 +51,6 @@ if [ $R -ne 0 ]; then
 	}
     }
     "
-    R=$?
-    if [ $R -ne 0 ]; then
-    	echo "Login failed"
-    	exit 1
-    fi
   else
     echo "ERROR: Not able to determine why Jotta cannot start:"
     jotta-cli status
